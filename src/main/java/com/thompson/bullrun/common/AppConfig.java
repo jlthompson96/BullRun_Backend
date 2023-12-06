1636d68d958120dcbb675020f283d09612e533ad
Joey Thompson <joeythompson1014@gmail.com>
2023-12-05 21:30:18 -0500

    Added Jacoco for code coverage.

--- src/main/java/com/thompson/bullrun/common/AppConfig.java
+++ src/main/java/com/thompson/bullrun/common/AppConfig.java
@@ -1,4 +1,4 @@
-package com.thompson.bullrun;
+package com.thompson.bullrun.common;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/thompson/bullrun/controller/TwelveDataController.java
+++ src/main/java/com/thompson/bullrun/controller/TwelveDataController.java
@@ -1,4 +1,4 @@
-package com.thompson.bullrun;
+package com.thompson.bullrun.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
