bfb28ae2cb73166ab9fdda0b6845679f3c2d38bc
Joey Thompson <joeythompson1014@gmail.com>
2024-03-01 15:04:12 -0500

    Refactor

--- src/main/java/com/thompson/bullrun/controller/TwelveDataController.java
+++ src/main/java/com/thompson/bullrun/controller/TwelveDataController.java
@@ -1,17 +1,13 @@
 package com.thompson.bullrun.controller;
 
-import com.thompson.bullrun.entities.CompanyInfoResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Map;
 
 @Slf4j
 @RestController
