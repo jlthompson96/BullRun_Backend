aa1ef0febcd351ff6b333e1a735fb8ed8bd18df0
Joey Thompson <joeythompson1014@gmail.com>
2024-03-22 10:46:55 -0400

    CORS Refactor

--- src/main/java/com/thompson/bullrun/common/AppConfig.java
+++ src/main/java/com/thompson/bullrun/common/AppConfig.java
@@ -3,6 +3,8 @@ package com.thompson.bullrun.common;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +12,19 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("") //Leave Blank to avoid multiple values in Access-Control-Allow-Origin
+                        .allowedOrigins("http://localhost:5173, https://coral-app-tozmc.ondigitalocean.app") // replace with your client's URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
