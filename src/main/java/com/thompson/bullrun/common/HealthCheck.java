fee86a1aac8c3a118af4a238ef82bc8cebaa017d
Joey Thompson <joeythompson1014@gmail.com>
2024-11-07 21:57:03 -0500

    Refactor

--- src/main/java/com/thompson/bullrun/common/HealthCheck.java
+++ src/main/java/com/thompson/bullrun/common/HealthCheck.java
@@ -9,26 +9,31 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 /**
- * This class is a REST controller that provides a health check endpoint for the service.
- * It uses the Lombok library for logging and Spring's RestController and GetMapping annotations to define the endpoint.
+ * REST controller that provides a health check endpoint for the service.
+ * Uses Lombok for logging and Spring's RestController and GetMapping annotations to define the endpoint.
  */
 @Slf4j
 @RestController
 public class HealthCheck {
 
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
     /**
-     * This method is mapped to the "/health" endpoint and returns a ResponseEntity with a message indicating the service is running.
-     * It also logs the current date and time, and the fact that the health check endpoint was called.
+     * Handles the "/health" endpoint to verify service health status.
+     * Logs the current date and time and indicates that the health check endpoint was called.
      *
      * @return ResponseEntity with a message indicating the service is running
      */
     @GetMapping("/health")
     public ResponseEntity<String> healthCheck() {
         LocalDateTime currentDateTime = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        String formattedDateTime = currentDateTime.format(formatter);
-        log.info("Calling health check endpoint");
-        log.info("Service is up and running as of {}", formattedDateTime);
-        return ResponseEntity.ok("Service is up and running as of " + formattedDateTime);
+        String formattedDateTime = currentDateTime.format(DATE_TIME_FORMATTER);
+
+        log.info("Health check endpoint called at {}", formattedDateTime);
+
+        String responseMessage = "Service is up and running as of " + formattedDateTime;
+        log.info("Health check status: {}", responseMessage);
+
+        return ResponseEntity.ok(responseMessage);
     }
 }
