3155e5ee837112f81dc8a4ebae10784dd4f47915
Joey Thompson <joeythompson1014@gmail.com>
2025-01-12 13:10:47 -0500

    Added Mongo log to logback

--- src/main/java/com/thompson/bullrun/common/MongoConfig.java
+++ src/main/java/com/thompson/bullrun/common/MongoConfig.java
@@ -3,16 +3,20 @@ package com.thompson.bullrun.common;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MongoConfig {
 
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+
     @Bean
     public MongoClient mongoClient() {
         MongoClientSettings settings = MongoClientSettings.builder()
-                .applyConnectionString(new com.mongodb.ConnectionString("mongodb+srv://bullrundev:saztAp-4figmo-byjqop@bullrundev.sexuf.mongodb.net/?retryWrites=true&w=majority&appName=BullRunDev"))
+                .applyConnectionString(new com.mongodb.ConnectionString(mongoUri))
                 .addCommandListener(new MongoCommandListener())
                 .build();
         return MongoClients.create(settings);
